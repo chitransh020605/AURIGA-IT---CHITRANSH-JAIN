a64aaac27a7372931f8fc81c3cade430d98a1b88
Chitransh Jain <chitransh0206jain@gmail.com>
2026-09-17 16:11:14 +0530

    Switch from better-sqlite3 to built-in node:sqlite (avoids native build tools requirement)

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const Database = require('better-sqlite3');
+const { DatabaseSync } = require('node:sqlite');
 const bcrypt = require('bcryptjs');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'tiffin.sqlite'));
-db.pragma('journal_mode = WAL');
-db.pragma('foreign_keys = ON');
+const db = new DatabaseSync(path.join(__dirname, 'tiffin.sqlite'));
+db.exec('PRAGMA journal_mode = WAL;');
+db.exec('PRAGMA foreign_keys = ON;');
 
 // ---------- SCHEMA ----------
 db.exec(`
